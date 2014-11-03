970e91269d6d231f3ba2e399dca9215b39636e8a
Anselm Christophersen <ac@anselm.dk>
2014-11-03 14:51:19 +0100

    work on assets import

--- lib/overwrite-current-site.sh
+++ lib/overwrite-current-site.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 #This script overwrites the current site
-#Only database is overwritten here - asset sync is handled by core
 #Called from sitesync-core
-#Needs to be supplied with database file and environment
+#Needs to be supplied with db name, files dir, and environment
 
-FILE=$1
-ENV=$2
+DBNAME=$1
+FILESDIR=$2
+ENV=$3
 
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../.. && pwd )";
 MODULEDIR="$BASEDIR/ttools-sitesync-wordpress";
-WPPATH="$BASEDIR/public/";
+WP_PATH="$BASEDIR/public";
 
 ENVVARS="$BASEDIR/ttools-core/lib/vars-for-env.sh $ENV"
 eval `$ENVVARS`
 
-cd $WPPATH;
+cd $WP_PATH;
 
-$ENV_PHPPATH $MODULEDIR/thirdparty/wp-cli.phar db import $FILE
+echo "Importing database...";
+
+$ENV_PHPPATH $MODULEDIR/thirdparty/wp-cli.phar db import $DBNAME
+
+echo "Importing files...";
+
+FILES_TO_IMPORT=$FILESDIR/public/content/uploads;
+
+
+
+rsync -avz --delete $FILES_TO_IMPORT $WP_PATH/content/uploads;
+
+
+
