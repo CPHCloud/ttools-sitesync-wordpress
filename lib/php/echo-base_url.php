7d68af52602538d7b29cab7ccbd190474d24c666
Anselm Christophersen <ac@anselm.dk>
2014-12-09 20:56:11 +0100

    echo base url legacy cupport

--- lib/php/echo-base_url.php
+++ lib/php/echo-base_url.php
@@ -1,4 +1,22 @@
 <?php
 include "sniff-environment.php";
 
-echo $env_config['base_url'];
+//LEGACY SUPPORT:
+
+
+if (isset($env_config['base_url'])) {
+	//this is the only one we need for the future
+	$baseUrl = $env_config['base_url'];
+	if (strlen($baseUrl) > 1) {
+		echo $baseUrl;
+	}
+
+};
+
+//this is the old one
+if (isset($env_vars['base_url'])) {
+	$baseUrl = $env_vars['base_url'];
+	if (strlen($baseUrl) > 1) {
+		echo $baseUrl;
+	}
+};
